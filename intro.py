0d926c45952f50c08e62a5a3a05fd4803e923ffb
ConnorAlchin <129019664+ConnorAlchin@users.noreply.github.com>
2023-07-26 23:39:18 +0000

    Added a yes/no and displayed instructions

--- intro.py
+++ intro.py
@@ -1,12 +1,13 @@
 print("welcome to a math quiz")
 
+
 #functios go here
 def yes_no(question):
     valid = False
     while not valid:
         response = input (question) .lower()
 
-
+        #Makes it so user can use each response for a yes/no response
         if response == "yes" or response == "y":
          response = "yes"
          return response
@@ -14,14 +15,19 @@ def yes_no(question):
         elif response == "no" or response == "n":
             response = "no"
             return response
-      
+        #makes sure user will say yes/no
         else:
            print("please awnser yes/no")
 
+#function to display instructions
+def instructions():
+  print("welcome to the math quiz")
+  print("Awnser as many questions as you can without losing all three of your lives")
 
-#maine routine goes here
-show_instructions = yes_no("Have you played the game before ")
-print("You chose {}".format(show_instructions))
-print()
-having_fun = yes_no("Are you having fun ")
-print("you said {} to having fun".format(having_fun))
+
+# *** MAIN ROUTINE ***
+# ask user if played before using yes no function
+played_before = yes_no("Have you played the game before? ")
+# if user hasn't played before instructions are displayed
+if played_before == "no":
+  instructions()
